2a0346eb52126c765f9cabbf063ddc00afbc571c
nasser.khosravi <jobnaserkhosravi@gmail.com>
2022-02-25 23:38:50 +0330

    Test extension function writeBool
    Other changes:
    Rename extension function name to prefix of extension type

--- espashgar/src/androidTest/java/khosravi/android/espashgar/ExtTest.kt
+++ espashgar/src/androidTest/java/khosravi/android/espashgar/ExtTest.kt
@@ -11,7 +11,7 @@ import org.mockito.Mockito.*
 class ExtTest {
 
     @Test
-    fun test_applyOn_margin() {
+    fun test_Margin_applyOn() {
         val margin = mock(Margin::class.java)
         val views = listOf(mock(View::class.java), mock(View::class.java))
         margin.applyOn(views)
@@ -21,7 +21,7 @@ class ExtTest {
     }
 
     @Test
-    fun test_applyOn_padding() {
+    fun test_Padding_applyOn() {
         val padding = mock(Padding::class.java)
         val views = listOf(mock(View::class.java), mock(View::class.java))
         padding.applyOn(views)
@@ -31,21 +31,21 @@ class ExtTest {
     }
 
     @Test
-    fun test_readBool_parcel_1() {
+    fun test_Parcel_readBool_1() {
         val mock = mock(Parcel::class.java)
         `when`(mock.readInt()).thenReturn(1)
         assertThat(mock.readBool()).isTrue()
     }
 
     @Test
-    fun test_readBool_parcel_0() {
+    fun test_Parcel_readBool_0() {
         val mock = mock(Parcel::class.java)
         `when`(mock.readInt()).thenReturn(0)
         assertThat(mock.readBool()).isFalse()
     }
 
     @Test
-    fun test_readBool_parcel_else() {
+    fun test_Parcel_readBool_else() {
         mock(Parcel::class.java).let { sut ->
             `when`(sut.readInt()).thenReturn(-1)
             assertThat(sut.readBool()).isNull()
@@ -58,21 +58,21 @@ class ExtTest {
     }
 
     @Test
-    fun test_writeBool_true_parcel() {
+    fun test_Parcel_writeBool_true() {
         val parcel = mock(Parcel::class.java)
         parcel.writeBool(true)
         verify(parcel).writeInt(1)
     }
 
     @Test
-    fun test_writeBool_false_parcel() {
+    fun test_Parcel_writeBool_false() {
         val parcel = mock(Parcel::class.java)
         parcel.writeBool(false)
         verify(parcel).writeInt(0)
     }
 
     @Test
-    fun test_writeBool_null_parcel() {
+    fun test_Parcel_writeBool_null() {
         val parcel = mock(Parcel::class.java)
         parcel.writeBool(null)
         verify(parcel).writeInt(-1)
